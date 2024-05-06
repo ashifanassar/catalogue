7154340adf0914e7bfd5a523977e4993fb8235e3
ashifanassar <ashifanassar@gmail.com>
2024-05-06 19:57:04 +0530

    jenkins-shared-library

--- mutable-infra/vars.tf
+++ mutable-infra/vars.tf
@@ -4,6 +4,14 @@ variable "COMPONENT" {
   
 }
 
+# Spot Variables
+variable "SPOT_INSTANCE_COUNT" {}
+variable "SPOT_INSTANCE_TYPE" {}
+
+# OD Variables
+variable "OD_INSTANCE_COUNT" {}
+variable "OD_INSTANCE_TYPE" {}
+
 
 # variable "ALB_NAME" {}
 
